c08c07b4d8b6420b5012ab599960e5562aa1add9
Ujjawal Kumar <2005@solvezy.com>
2021-07-28 21:47:51 +0530

    portfolio init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import portfolioImage from './portfolio.PNG';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={portfolioImage} className="App-logo" alt="logo" />
+        <p>Ujjwal Kaushik</p>
+        <p>Senior FrontEnd Developer | SDE-III</p>
       </header>
     </div>
   );
